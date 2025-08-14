f24cada75c8d5e4e337d66c378fc818ad20a9068
cloco-bhupesh <shakya.bhupesh@cloco.net>
2025-08-14 17:36:11 +0545

    apple

--- core/models.py
+++ core/models.py
@@ -1,18 +1,3 @@
 # from django.db import models  # pylint: disable=unused-variable
-from django.db import models
-
 
 # Create your models here.
-class Apple(models.Model):
-    name = models.CharField(
-        max_length=100,
-    )
-    color = models.CharField(
-        max_length=50,
-    )
-
-    def __str__(self):
-        return f"""{self.name}
-
-
-            ({self.color})"""
